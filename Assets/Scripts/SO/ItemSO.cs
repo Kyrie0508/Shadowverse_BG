712d16e63ff9cee93c8a235823b0e4863464e0e5
Kyrie0508 <ssss3044@gmail.com>
2024-11-28 06:13:03 +0900

    add scripts, assets and environment setting

---
+++ Assets/Scripts/SO/ItemSO.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+ 
+
+[System.Serializable]
+
+public class Item {
+
+    public string name;
+
+    public int attack;
+
+    public int health;
+
+    public Sprite sprite;
+    
+}
+
+ 
+
+[CreateAssetMenu(fileName = "ItemSO", menuName = "Scriptable Object/ItemSO")]
+
+public class ItemSO : ScriptableObject {
+
+    public Item[] items;
+
+}
